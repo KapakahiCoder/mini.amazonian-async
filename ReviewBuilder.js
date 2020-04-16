4d119b80abe23a6ae7c8ba050561794e9c102c69
Kapakahi <rubbishman@gmail.com>
2020-04-16 21:39:40 +0900

    Completed Async and promise

--- ReviewBuilder.js
+++ ReviewBuilder.js
@@ -28,11 +28,38 @@ class ReviewBuilder {
   }
 
   buildReviewsPromises() {
-    // FIXME
+    return Promise.all([
+      readFile("./data/products.json"),
+      readFile("./data/reviews.json"),
+      readFile("./data/users.json"),
+    ]).then((files) => {
+      const products = JSON.parse(files[0]);
+      const reviews = JSON.parse(files[1]);
+      const users = JSON.parse(files[2]);
+
+      const data = {};
+      data.products = products;
+      data.reviews = reviews;
+      data.users = users;
+      return produceResult(data);
+    });
   }
 
   async buildReviewsAsyncAwait() {
-    // FIXME
+    const products = await readFile("./data/products.json");
+    const reviews = await readFile("./data/reviews.json");
+    const users = await readFile("./data/users.json");
+
+    const JSONproducts = JSON.parse(products);
+    const JSONreviews = JSON.parse(reviews);
+    const JSONusers = JSON.parse(users);
+
+    const data = {};
+    data.products = JSONproducts;
+    data.reviews = JSONreviews;
+    data.users = JSONusers;
+
+    return produceResult(data);
   }
 }
 
